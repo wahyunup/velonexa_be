7c9ad34e85b823f11b8004e6155ab28a5774f6ac
aaldiiieee <prmdy02@gmail.com>
2025-07-17 20:59:29 +0700

    feat: implement real-time notification system with socket.io and create notification routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,16 @@ import feedRoutes from "../routes/feedRoutes.js";
 import userRoutes from "../routes/userRoutes.js";
 import commentRoutes from "../routes/commentRoutes.js";
 import RefreshToken from "../routes/refreshTokenRoutes.js";
+import notificationRoutes from "../routes/notificationRoutes.js";
 
 const router = express.Router();
 
-router.use(feedRoutes, userRoutes, RefreshToken, commentRoutes);
+router.use(
+  feedRoutes,
+  userRoutes,
+  RefreshToken,
+  commentRoutes,
+  notificationRoutes
+);
 
 export default router;
