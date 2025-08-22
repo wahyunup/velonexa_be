b1719a4a486995b06e5764d2587228e8c306eca7
wahyunup <wahyunurpambuko@gmail.com>
2025-08-22 22:30:29 +0700

    bismillah bisaaaaa

--- src/index.js
+++ src/index.js
@@ -38,10 +38,10 @@ app.use(express.json());
 
 app.use(router);
 
-// const port = process.env.PRODUCTION_URL ||  3001;
-// app.listen(port, () => {
-//   console.log("welcome to velonexa 🤠");
-//   console.log(`velonexa is running on port ${port} 🚀`);
-// });
+const port = process.env.PRODUCTION_URL ||  3001;
+app.listen(port, () => {
+  console.log("welcome to velonexa 🤠");
+  console.log(`velonexa is running on port ${port} 🚀`);
+});
 
 export default app;
