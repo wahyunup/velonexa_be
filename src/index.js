fdc525518fa90607485b696ae582f4fddeb4030b
wahyunup <wahyunurpambuko@gmail.com>
2026-01-28 09:53:20 +0700

    config cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ import cookieParser from "cookie-parser";
 // import { socketHandler } from "../socket/index.js";
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://velonexa.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "https://velonexa.vercel.app",
+    "http://localhost:3000",
+  ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
